a5caea5b53200aa5d2578f64c4779557f2f7a336
hemant-hari <hemant-hari@users.noreply.github.com>
2020-12-21 13:33:23 +0000

    copying game of life - rust wasm doc

---
+++ www/index.js
@@ -0,0 +1,107 @@
+import { Display, Cell } from "wasm-chip8";
+import { memory } from "wasm-chip8/wasm_chip8_bg";
+
+const CELL_SIZE = 20; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#333333";
+const ALIVE_COLOR = "#41FF00";
+
+// Construct the display, and get its width and height.
+const display = Display.new_empty();
+const width = display.width();
+const height = display.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+  display.tick();
+
+  drawGrid();
+  drawCells();
+
+  setTimeout(100);
+
+  requestAnimationFrame(renderLoop);
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = display.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+renderLoop();
+
+
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        leftPressed = false;
+    }
+}
